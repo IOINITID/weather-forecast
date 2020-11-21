2fbc6d11fd6766f408408fb96c428acceaa07495
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-11-21 22:50:21 +0300

    Added service file

---
+++ src/services/weatherForecastService.js
@@ -0,0 +1,8 @@
+class WeatherForecastService {
+  constructor() {
+    this.apiKey = `0cf9471e82e09b7e34f9ce007f3bbb0d`;
+    this.apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=`;
+  }
+}
+
+export default WeatherForecastService;
