d8f7fa4be2af6cf277571f2e959f58c1abc2c51b
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-11-23 18:06:56 +0300

    Added loader component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import WeatherForecastService from '../../services/weatherForecastService';
+import Loader from '../loader/loader';
 import './app.css';
 
 const weatherService = new WeatherForecastService();
@@ -7,6 +8,7 @@ const weatherService = new WeatherForecastService();
 const App = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState(``);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getWeatherByCityName = async (cityName) => {
     const response = await weatherService.getWeatherByCityName(cityName);
@@ -16,6 +18,7 @@ const App = () => {
   const getWeatherByGeolocation = async (lat, lon) => {
     const response = await weatherService.getWeatherByGeolocation(lat, lon);
     setWeatherData(response);
+    setIsLoading(false);
   };
 
   const getWeatherByCurrentGeolocation = () => navigator.geolocation.getCurrentPosition((position) => {
@@ -31,6 +34,10 @@ const App = () => {
     }
   }, [city]);
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div className="container">
       <input
@@ -40,32 +47,18 @@ const App = () => {
         onChange={(evt) => setCity(evt.target.value)}
       />
       {
+        weatherData &&
         <div className="weather">
-          {
-            weatherData &&
-            <img
-              className="weather__icon"
-              src={`http://openweathermap.org/img/wn/${weatherData && weatherData.weather[0].icon}@4x.png`}
-              width="100"
-              height="100"
-            />
-
-          }
-          <p className="weather__temperature">
-            {
-              weatherData && `${weatherData.main.temp.toFixed(0)}°C`
-            }
-          </p>
-          <p className="weather__city">
-            {
-              weatherData && weatherData.name
-            }
-          </p>
-          <p className="weather__description">
-            {
-              weatherData && weatherData.weather[0].description
-            }
-          </p>
+          <img
+            className="weather__icon"
+            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
+            width="100"
+            height="100"
+          />
+          <p className="weather__temperature">{`${weatherData.main.temp.toFixed(0)}°C`}</p>
+          <p className="weather__city">{weatherData.name}</p>
+          <p className="weather__description">{weatherData.weather[0].description}</p>
+          <p className="weather__feels-like">Ощущается как: {weatherData.main.feels_like.toFixed(0)}°C</p>
         </div>
       }
     </div>
---
+++ src/components/loader/loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoaderIcon from '../../assets/images/loader-icon.svg';
+
+const Loader = () => {
+  return (
+    <LoaderIcon
+      className="loader"
+      width="100"
+      height="100"
+    />
+  );
+};
+
+export default Loader;
