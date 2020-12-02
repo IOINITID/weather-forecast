8e047344727ebb6a0798c4d7b598368249284a72
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-12-02 21:51:40 +0300

    Updated services file

--- src/services/weatherForecastService.ts
+++ src/services/weatherForecastService.ts
@@ -1,27 +1,26 @@
-interface IWeatherForecastService {
-  apiKey: string;
-  apiUrl: string;
-}
-
-class WeatherForecastService implements IWeatherForecastService {
-  apiKey: string;
-  apiUrl: string;
+class WeatherForecastService {
+  private apiKey: string;
+  private apiUrl: string;
 
   constructor() {
     this.apiKey = `0cf9471e82e09b7e34f9ce007f3bbb0d`;
     this.apiUrl = `https://api.openweathermap.org/data/2.5/weather?`;
   }
 
-  getWeatherByCityName(city: string) {
-    return fetch(`${this.apiUrl}q=${city}&units=metric&lang=ru&appid=${this.apiKey}`)
-    .then((response) => response.ok && response.json())
-    .catch((error) => error);
+  public getWeatherByCityName(city: string): Promise<any> {
+    const fetchUrl: string = `${this.apiUrl}q=${city}&units=metric&lang=ru&appid=${this.apiKey}`;
+
+    return fetch(fetchUrl)
+      .then((response) => response.ok && response.json())
+      .catch((error) => error);
   }
 
-  getWeatherByGeolocation(lat: number, lon: number) {
-    return fetch(`${this.apiUrl}lat=${lat}&lon=${lon}&lang=ru&units=metric&appid=${this.apiKey}`)
-    .then((response) => response.ok && response.json())
-    .catch((error) => error);
+  public getWeatherByGeolocation(lat: number, lon: number): Promise<any> {
+    const fetchUrl: string = `${this.apiUrl}lat=${lat}&lon=${lon}&lang=ru&units=metric&appid=${this.apiKey}`;
+
+    return fetch(fetchUrl)
+      .then((response) => response.ok && response.json())
+      .catch((error) => error);
   }
 }
 
