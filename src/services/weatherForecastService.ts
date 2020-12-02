82d5e527b17c88fa0a86c4b8734fac156d42d3bc
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-12-02 09:28:36 +0300

    Updated services file with TypeScript

--- src/services/weatherForecastService.ts
+++ src/services/weatherForecastService.ts
@@ -1,4 +1,12 @@
-class WeatherForecastService {
+interface IWeatherForecastService {
+  apiKey: string;
+  apiUrl: string;
+}
+
+class WeatherForecastService implements IWeatherForecastService {
+  apiKey: string;
+  apiUrl: string;
+
   constructor() {
     this.apiKey = `0cf9471e82e09b7e34f9ce007f3bbb0d`;
     this.apiUrl = `https://api.openweathermap.org/data/2.5/weather?`;
