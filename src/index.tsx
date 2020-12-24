74740833d28c1646a88d228dbc99fefd7581a683
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-12-24 23:09:12 +0300

    Updated index.ts file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
-const Root: React.FC = () => {
+const Root = () => {
   return (
     <App />
   );
