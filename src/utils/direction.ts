2f6a9d33a136d5bf48208666aa86aa65e15c82a0
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-12-25 00:07:18 +0300

    Added direction.ts file

---
+++ src/utils/direction.ts
@@ -0,0 +1,42 @@
+const getDirectionDescription = (direction: string): string => {
+  switch (direction) {
+    case `NNE`:
+      return `ССВ`;
+    case `NE`:
+      return `СВ`;
+    case `ENE`:
+      return `ВСВ`;
+    case `E`:
+      return `В`;
+    case `ESE`:
+      return `ВЮВ`;
+    case `SE`:
+      return `ЮВ`;
+    case `SSE`:
+      return `ЮЮВ`;
+    case `S`:
+      return `Ю`;
+    case `SSW`:
+      return `ЮЮЗ`;
+    case `SW`:
+      return `ЮЗ`;
+    case `WSW`:
+      return `ЗЮЗ`;
+    case `W`:
+      return `З`;
+    case `WNW`:
+      return `ЗСЗ`;
+    case `NW`:
+      return `СЗ`;
+    case `NNW`:
+      return `ССЗ`;
+    case `N`:
+      return `С`;
+    default:
+      return `С`;
+  }
+};
+
+export {
+  getDirectionDescription
+};
