85f0db1e654d5fc64f0d14f6ed2a5357dc57ee55
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-01-13 16:12:14 +0300

    Revert "Handled geolacation errors"
    This reverts commit 85eb533b7a3547c0de55a35b1438904eba090f53.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -51,6 +51,7 @@ const App = () => {
   const getWeatherByGeolocation = (lat: number, lon: number): void => {
     weatherService.getWeatherByGeolocation(lat, lon).then((response) => {
       setWeatherData((prev) => [response, ...prev]);
+      setIsLoading(false);
       setActiveCard(true);
     });
   };
@@ -62,6 +63,7 @@ const App = () => {
 
     const positionError = (positionError: IPositionError): void => {
       console.log(positionError);
+      setIsLoading(false);
     };
 
     const positionOptions: IPositionOptions = {
