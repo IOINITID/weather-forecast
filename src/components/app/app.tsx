06ebd12e7859f7a1ad1dd7ddeb0bd9c550732312
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-01-13 15:16:18 +0300

    Updated app component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -113,6 +113,7 @@ const App = () => {
   return (
     <motion.div className="app" variants={containerMotion} initial="hidden" animate="visible">
       {weatherData &&
+        weatherData.length > cities.length &&
         weatherData.map((item, index) => {
           const temperature: string = item.main.temp.toFixed(0);
           const weatherCity: string = item.name;
